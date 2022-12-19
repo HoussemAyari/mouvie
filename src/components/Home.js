14e0247e612fd717cf7579383a0b0fac605d030b
HoussemAyari <toopanga@gmail.com>
2022-12-19 21:57:59 +0100

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import FilmCrd from "./FilmCrd";
-import "./List.css";
-
-function List({ films, search, Rate }) {
+import Searchbar from "./Searchbar";
+import { Link } from "react-router-dom";
+function Home({films, Rate, search, searchForFilm, starSearch, setRate, addFilm}) {
   return (
-    <div
+    <div>
+      <Searchbar searchForFilm={searchForFilm} starSearch={starSearch} setRate={setRate}  addFilm={addFilm}/>
+      <div
       style={{
         display: "flex",
         flexDirection: "row",
@@ -17,11 +19,15 @@ function List({ films, search, Rate }) {
       {films.film
         .filter((elt) => elt.title.toUpperCase().includes(search.toUpperCase()) && elt.rate>=Rate)
         .map((elt) => (
-          <FilmCrd key={elt.id} film={elt}  />
+          <Link to={`/details/${elt.id}`}>
+        <FilmCrd key={elt.id} film={elt}  />
+              </Link>
+
         ))}
         
+    </div>
     </div>
   );
 }
 
-export default List;
+export default Home;
